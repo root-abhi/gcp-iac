6130b280cff52f45d916cbaa0fd76f10fc0d1dd5
Abhishek Chauhan <abhchauh6@WKMZT31E3DC6.global.publicisgroupe.net>
2023-07-05 13:52:22 +0100

    compute instance

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "var.google_credentials"
+  credentials = var.google_credentials
   project = "learning-384823"
   region  = "europe-west1"
   zone    = "europe-west1-b"
